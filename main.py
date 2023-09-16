007bf281eff4554b2c2030d71f3184ad9aa4742e
Eduardov35 <144554640+Eduardov35@users.noreply.github.com>
2023-09-16 08:33:18 -0500

    Create main.py
    a

---
+++ main.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma"
+            }
+for i in range(5):
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("Tu palabra", word, "no esta en el diccionario")
